41965ac96d8fcc4be9243315954ffc1c15aaf338
freniecki <247773@edu.p.lodz.pl>
2025-03-19 22:47:51 +0100

    removed unused

--- src/test/java/cps/model/SignalFactoryTest.java
+++ src/test/java/cps/model/SignalFactoryTest.java
@@ -5,22 +5,9 @@ import org.junit.jupiter.api.Test;
 class SignalFactoryTest {
 
     @Test
-    void createSineSignal() {
-        Signal sine = SignalFactory.createSineSignal();
-
-        System.out.println(sine.getSampleSize());
-        System.out.println(sine.getSampleTime());
-        System.out.println(sine.getAmplitude());
-
-        for (double sample : sine.samples) {
-            System.out.println(sample);
-        }
-    }
-
-    @Test
     void generateUniform() {
         for (int i = 0; i < 5; i++) {
-            System.out.println(SignalFactory.generateUniform(1.0));
+            System.out.println(SignalFactory.getUniformValue(1.0));
         }
     }
 
