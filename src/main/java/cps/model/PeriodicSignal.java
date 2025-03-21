a5148fd0405f7506cb839c30ed16a9cc330b5e2b
freniecki <247773@edu.p.lodz.pl>
2025-03-21 23:08:59 +0100

    stats moved to dedicated class

--- src/main/java/cps/model/PeriodicSignal.java
+++ src/main/java/cps/model/PeriodicSignal.java
@@ -13,25 +13,25 @@ import java.util.List;
 public class PeriodicSignal extends Signal {
     double period;
 
-    @Override
-    public List<Double> getSamplesForCalculation() {
-        // Oblicz liczbę pełnych okresów
-        double timeSpan = durationTime - startTime;
-        int fullPeriods = (int)(timeSpan / period);
-
-        // Jeśli nie ma pełnych okresów, zwróć pustą listę
-        if (fullPeriods <= 0) return new ArrayList<>();
-
-        // Oblicz liczbę próbek w jednym okresie
-        double samplesPerPeriod = period / SignalFactory.SAMPLE_STEP;
-        int fullSamplePeriods = (int)samplesPerPeriod;
-
-        // Utwórz listę zawierającą tylko pełne okresy
-        List<Double> fullPeriodSamples = new ArrayList<>();
-        for (int i = 0; i < fullPeriods * fullSamplePeriods && i < samples.size(); i++) {
-            fullPeriodSamples.add(samples.get(i));
-        }
-
-        return fullPeriodSamples;
-    }
+//    @Override
+//    public List<Double> getSamplesForCalculation() {
+//        // Oblicz liczbę pełnych okresów
+//        double timeSpan = durationTime - startTime;
+//        int fullPeriods = (int)(timeSpan / period);
+//
+//        // Jeśli nie ma pełnych okresów, zwróć pustą listę
+//        if (fullPeriods <= 0) return new ArrayList<>();
+//
+//        // Oblicz liczbę próbek w jednym okresie
+//        double samplesPerPeriod = period / SignalFactory.SAMPLE_STEP;
+//        int fullSamplePeriods = (int)samplesPerPeriod;
+//
+//        // Utwórz listę zawierającą tylko pełne okresy
+//        List<Double> fullPeriodSamples = new ArrayList<>();
+//        for (int i = 0; i < fullPeriods * fullSamplePeriods && i < samples.size(); i++) {
+//            fullPeriodSamples.add(samples.get(i));
+//        }
+//
+//        return fullPeriodSamples;
+//    }
 }
