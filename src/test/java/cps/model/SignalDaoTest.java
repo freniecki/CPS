b2c1f21a1d6f8963510effbb4506b0b06ccf6ba0
freniecki <247773@edu.p.lodz.pl>
2025-04-01 09:01:26 +0200

    read/write serialized signal

---
+++ src/test/java/cps/model/SignalDaoTest.java
@@ -0,0 +1,39 @@
+package cps.model;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.Map;
+
+class SignalDaoTest {
+
+    @Test
+    void readSignalFromFile() {
+        Signal signal = SignalDao.readSignalFromFile("signals/2025-03-31_21:29:12.990266305.ser");
+
+        System.out.println(signal.getSignalType());
+        for (Map.Entry<Double, Double> entry : signal.getTimestampSamples().entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+    }
+
+    @Test
+    void writeSignalToFile() {
+
+        PeriodicSignal signal = PeriodicSignal.builder()
+                .signalType(SignalType.SINE)
+                .amplitude(1.0)
+                .startTime(0.0)
+                .durationTime(5.0)
+                .period(1.0)
+                .build();
+
+        try {
+            SignalDao.writeSignalToFile(signal);
+        } catch (IOException e) {
+            System.out.print("no sie nie udało, sorry");
+            throw new RuntimeException(e);
+        }
+
+    }
+}
