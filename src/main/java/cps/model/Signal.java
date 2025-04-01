1a06eb0d071ce4ed54722e18c7a6dd6e11a325ae
freniecki <247773@edu.p.lodz.pl>
2025-04-01 09:01:55 +0200

    serializable to io file

--- src/main/java/cps/model/Signal.java
+++ src/main/java/cps/model/Signal.java
@@ -3,12 +3,13 @@ package cps.model;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
+import java.io.Serializable;
 import java.util.*;
 
 @Data
 @AllArgsConstructor
 @SuperBuilder
-public class Signal {
+public class Signal implements Serializable {
     double amplitude;
     double startTime;
     double durationTime;
