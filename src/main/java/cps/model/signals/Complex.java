ad12d233a88a2e3e942d8d798e64e10e85141ec6
freniecki <247773@edu.p.lodz.pl>
2025-06-11 13:34:33 +0200

    complex as class, access method needed for optimization

--- src/main/java/cps/model/signals/Complex.java
+++ src/main/java/cps/model/signals/Complex.java
@@ -1,23 +1,57 @@
 package cps.model.signals;
 
-public record Complex(double real, double imaginary) {
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Setter
+public final class Complex {
+    private double real;
+    private double imaginary;
+
+    public Complex(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public double real() {
+        return real;
+    }
+
+    public double imaginary() {
+        return imaginary;
+    }
 
     public Complex plus(Complex other) {
-        return new Complex(this.real + other.real,this.imaginary + other.imaginary);
+        return new Complex(this.real + other.real, this.imaginary + other.imaginary);
     }
 
     public Complex minus(Complex other) {
-        return new Complex(this.real - other.real,this.imaginary - other.imaginary);
+        return new Complex(this.real - other.real, this.imaginary - other.imaginary);
     }
 
     public Complex times(Complex other) {
-        double real = this.real * other.real - this.imaginary * other.imaginary;
-        double imaginary = this.real * other.imaginary + this.imaginary * other.real;
-        return new Complex(real, imaginary);
+        return new Complex(this.real * other.real - this.imaginary * other.imaginary,
+                this.real * other.imaginary + this.imaginary * other.real);
     }
 
     @Override
     public String toString() {
         return "Complex{R=%.3f,I=%.3f}".formatted(real, imaginary);
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (Complex) obj;
+        return Double.doubleToLongBits(this.real) == Double.doubleToLongBits(that.real) &&
+                Double.doubleToLongBits(this.imaginary) == Double.doubleToLongBits(that.imaginary);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(real, imaginary);
+    }
+
 }
